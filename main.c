f7c7e98b1b3c12f1d5d01fa1c514f7992244f551
Mateusz Odrobiński <mateusz.odrobinski@nexteer.com>
2024-05-06 23:29:53 +0200

    Krok 1. Aktualna godzina

--- main.c
+++ main.c
@@ -3,7 +3,18 @@
 
 int main()
 {
+	time_t timeNow;
+	struct tm * timeinfo;
 
+	char timeText[80];
+
+	timeNow = time(NULL);
+	timeinfo = localtime (&timeNow);
+	strftime(timeText, sizeof(timeText),"%H:%M:%S",timeinfo);
+
+	printf("Godzina: %s", timeText);
+
+	fflush(stdout);
 
 	return 0;
 }
