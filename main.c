3f7e5dad53fee5e485cf86dad220b5b7013dd269
Mateusz Odrobiński <mateusz.odrobinski@nexteer.com>
2024-05-06 23:57:08 +0200

    Krok 2. Proste odliczanie czasu

--- main.c
+++ main.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
 #include <time.h>
+#include <unistd.h>
 
 int main()
 {
-	time_t timeNow;
+	time_t timeNow, timeStart, diffTime;
 	struct tm * timeinfo;
 
 	char timeText[80];
 
-	timeNow = time(NULL);
-	timeinfo = localtime (&timeNow);
+	timeStart = time(NULL);
+	timeinfo = localtime (&timeStart);
 	strftime(timeText, sizeof(timeText),"%H:%M:%S",timeinfo);
 
-	printf("Godzina: %s", timeText);
+	printf("Godzina: %s\n", timeText);
+
+	for(int i=0; i<5; i++)
+	{
+		timeNow = time(NULL);
+		diffTime = timeNow - timeStart;
+		timeinfo = gmtime(&diffTime);
+		strftime (timeText, sizeof(timeText), "%H:%M:%S",timeinfo);
+		printf("Czas pracy: %s\n", timeText);
+
+		sleep(1);
+	}
 
 	fflush(stdout);
 
